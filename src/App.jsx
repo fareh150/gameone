a811ea97764eb89e8af5ecde1b9c71f0b1dae5d1
fareh <fareh.rv@gmail.com>
2023-10-18 14:19:12 +0200

    continue CG

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 
 import { Outlet } from 'react-router-dom'
+import { useEffect, useState } from "react"
+import axios from "axios"
 import './App.css'
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
 
 function App() {
+
+  const [allData, setAllData] = useState([])
+
+
+  const getData = async () => {
+      const data = await axios.get(`https://restcountries.com/v3.1/all`)
+      console.log(data.data)
+  }
+
+  useEffect(() => {
+      getData()
+  },[])
 
   return (
     <>
--- src/pages/Game2/Game2.jsx
+++ src/pages/Game2/Game2.jsx
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react"
 import "./Game2.css"
+import axios from "axios"
 
 const Game2 = () => {
+
+
     return(
         <main className="gametwo">
             <h2 className="title">Country Game</h2>
+            <section className="boardbox">
+                <div className="points">current rate : 0</div>
+                <article className="playbox"></article>
+            </section>
         </main>
     )
 }
